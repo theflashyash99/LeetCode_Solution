5c5edf4fbc6552acd5c3c34cab4079e11148722a
Yash Jaiswal <100846396+theflashyash99@users.noreply.github.com>
2026-06-25 12:10:39 +0530

    Time: 0 ms (100%), Space: 56.2 MB (60.97%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -2,24 +2,35 @@
  * @param {number[]} nums(
  * @return {number}
  */
-var singleNumber = function(nums) {
-    let hash = {};
+// var singleNumber = function(nums) {
+//     let hash = {};
 
 
-    for(let i = 0 ; i<nums.length;i++){
-        if(!hash[nums[i]]){
-            hash[nums[i]]=1
-        }else{
-            hash[nums[i]]++
-        }
+//     for(let i = 0 ; i<nums.length;i++){
+//         if(!hash[nums[i]]){
+//             hash[nums[i]]=1
+//         }else{
+//             hash[nums[i]]++
+//         }
+//     }
+
+//     for(let i = 0 ; i<nums.length;i++){
+//         if(hash[nums[i]]==1){
+//             return nums[i]
+//         }
+        
+        
+//         }
+// }
+
+
+// better approach
+
+var singleNumber = function (nums){
+    let xor = 0 ;
+    for(let i=0 ; i<nums.length ; i++){
+        xor = xor ^ nums[i]
     }
 
-    for(let i = 0 ; i<nums.length;i++){
-        if(hash[nums[i]]==1){
-            return nums[i]
-        }
-        
-        
-        }
+    return xor;
 }
-
